5c98b7170d5a4902ff44bade6819b95fbaa6d6c0
Gabriel Jardim <gabrielrlj99@outlook.com>
2020-06-29 11:55:04 -0300

    adicionando mais estilos

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import { ProdutoService } from './service/ProdutoService';
 import {DataTable} from 'primereact/datatable';
 import {Column} from 'primereact/column';
 
+import {Panel} from 'primereact/panel';
+
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+
 export default class App extends Component{
   constructor(){
     super();
@@ -18,12 +25,14 @@ export default class App extends Component{
 
   render(){
     return(
-      <DataTable value={this.state.produtos}>
-        <Column field="id" header="ID"></Column>
-        <Column field="nome" header="NOME"></Column>  
-        <Column field="quantidade" header="QUANTIDADE"></Column>
-        <Column field="valor" header="VALOR"></Column> 
-      </DataTable>
+      <Panel header="React CRUD App" style={{width:'80%',margin: '0 auto', marginTop: '20px'}}>
+        <DataTable value={this.state.produtos}>
+          <Column field="id" header="ID"></Column>
+          <Column field="nome" header="NOME"></Column>  
+          <Column field="quantidade" header="QUANTIDADE"></Column>
+          <Column field="valor" header="VALOR"></Column> 
+        </DataTable>
+      </Panel>
     );
   }
 
